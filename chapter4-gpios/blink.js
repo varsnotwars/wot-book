e9647695d76afbecd287c7e6e135462cb6a00bde
varsnotwars <luiss@hush.ai>
2017-05-13 03:02:09 +1000

    Revert "replaced onoff with pi-gpio"
    This reverts commit 806fea49d6b2d11d69985801acf01f2a761ef485.

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -1,31 +1,25 @@
-var gpio = require('pi-gpio'); //#A
-var interval;
+var onoff = require('onoff'); //#A
 
-gpio.open(4, "output", function (err) { //#B
-  if(err) console.log('error during initial open:', err);
-  gpio.write(4, 1, function(err) {
-    if(err) console.log('error during initial write:', err);
-    console.log()
-  });
-});
+var Gpio = onoff.Gpio,
+  led = new Gpio(4, 'out'), //#B
+  interval;
 
 interval = setInterval(function () { //#C
   var value = (led.readSync() + 1) % 2; //#D
-  gpio.write(4, value, function(err) { //#E
-    if(err) console.log('error during timeout write:', err);
+  led.write(value, function() { //#E
     console.log("Changed LED state to: " + value);
   });
 }, 2000);
 
 process.on('SIGINT', function () { //#F
   clearInterval(interval);
-  gpio.write(4, 0); //#G
-  gpio.close(4);
+  led.writeSync(0); //#G
+  led.unexport();
   console.log('Bye, bye!');
   process.exit();
 });
 
-// #A Import the pi-gpio library
+// #A Import the onoff library
 // #B Initialize pin 4 to be an output pin
 // #C This interval will be called every 2 seconds
 // #D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
