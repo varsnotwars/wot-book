806fea49d6b2d11d69985801acf01f2a761ef485
varsnotwars <luiss@hush.ai>
2017-05-13 02:23:54 +1000

    replaced onoff with pi-gpio

--- chapter4-gpios/blink.js
+++ chapter4-gpios/blink.js
@@ -1,25 +1,31 @@
-var onoff = require('onoff'); //#A
+var gpio = require('pi-gpio'); //#A
+var interval;
 
-var Gpio = onoff.Gpio,
-  led = new Gpio(4, 'out'), //#B
-  interval;
+gpio.open(4, "output", function (err) { //#B
+  if(err) console.log('error during initial open:', err);
+  gpio.write(4, 1, function(err) {
+    if(err) console.log('error during initial write:', err);
+    console.log()
+  });
+});
 
 interval = setInterval(function () { //#C
   var value = (led.readSync() + 1) % 2; //#D
-  led.write(value, function() { //#E
+  gpio.write(4, value, function(err) { //#E
+    if(err) console.log('error during timeout write:', err);
     console.log("Changed LED state to: " + value);
   });
 }, 2000);
 
 process.on('SIGINT', function () { //#F
   clearInterval(interval);
-  led.writeSync(0); //#G
-  led.unexport();
+  gpio.write(4, 0); //#G
+  gpio.close(4);
   console.log('Bye, bye!');
   process.exit();
 });
 
-// #A Import the onoff library
+// #A Import the pi-gpio library
 // #B Initialize pin 4 to be an output pin
 // #C This interval will be called every 2 seconds
 // #D Synchronously read the value of pin 4 and transform 1 to 0 or 0 to 1
