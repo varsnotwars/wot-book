7cfa87526505e73165e1b8c44f898ade2e5f97e9
varsnotwars <luiss@hush.ai>
2017-05-13 18:45:55 +1000

    PIR change to trigger LED
    Modified pir.js to toggle LED state

--- chapter4-gpios/pir.js
+++ chapter4-gpios/pir.js
@@ -1,14 +1,30 @@
-var Gpio = require('onoff').Gpio,
-  sensor = new Gpio(17, 'in', 'both');    //#A
+var Gpio = require('onoff').Gpio;
+var sensor = new Gpio(17, 'in', 'both');    //#A
+var led = new Gpio(4, 'out');
 
 sensor.watch(function (err, value) { //#B
   if (err) exit(err);
-  console.log(value ? 'there is someone!' : 'not anymore!');
+  if(value) {
+    var ledOnState = 1;
+    console.log('body detected');
+    led.write(ledOnState, function() {
+      console.log('LED state: ' + ledOnState);
+    });
+  } else {
+    var ledOffState = 0;
+    led.write(ledOffState, function() {
+      console.log('LED state: ' + ledOffState);
+    });
+  }
 });
 
 function exit(err) {
   if (err) console.log('An error occurred: ' + err);
+  // sensor
   sensor.unexport();
+  // led
+  led.writeSync(0);
+  led.unexport();
   console.log('Bye, bye!')
   process.exit();
 }
